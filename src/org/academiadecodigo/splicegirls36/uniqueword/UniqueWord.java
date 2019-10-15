6a7c3e4c5b5910a8d4ca4779c1f53d854c4e1613
Vitor Ferreira <vitor.dominor@gmail.com>
2019-10-15 01:00:55 +0100

    Solved exercise. Thought wrong before. Fuck this shit... only a few lines of code. These MCs...

--- src/org/academiadecodigo/splicegirls36/uniqueword/UniqueWord.java
+++ src/org/academiadecodigo/splicegirls36/uniqueword/UniqueWord.java
@@ -1,32 +1,19 @@
 package org.academiadecodigo.splicegirls36.uniqueword;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 
 public class UniqueWord implements Iterable<String> {
 
-    private String[] words;
+    private HashSet<String> words;
 
     public UniqueWord(String string) {
-        this.words = string.split(" ");
+        this.words = new HashSet<>(Arrays.asList(string.split(" ")));
     }
 
     @Override
     public Iterator<String> iterator() {
-        return new UniqueWordsIterator();
-    }
-
-    private class UniqueWordsIterator implements Iterator<String> {
-
-        private int counter = -1;
-
-        @Override
-        public boolean hasNext() {
-            return counter < words.length - 1;
-        }
-
-        @Override
-        public String next() {
-            return words[++counter];
-        }
+        return words.iterator();
     }
 }
